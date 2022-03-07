5c53284ccd5a3fe7bdcb9b9b44f481b90ebee299
quentin <quentin.klebaur@epitech.eu>
2022-03-07 13:06:32 -0500

    add Qt

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 cmake -BImageCompressor -S. /y
 xcopy /e /k /h /i include\ ImageCompressor\include\ /y
 xcopy /e /k /h /i src\ ImageCompressor\src\ /y
-xcopy .\Qt\lib\Qt5Gui.dll .\ImageCompressor\Release\ /y
-xcopy .\Qt\lib\Qt5Core.dll .\ImageCompressor\Release\ /y
-xcopy .\Qt\lib\Qt5Widgets.dll .\ImageCompressor\Release\ /y
+xcopy .\Qt\bin\Qt5Gui.dll .\ImageCompressor\Release\ /y
+xcopy .\Qt\bin\Qt5Core.dll .\ImageCompressor\Release\ /y
+::xcopy .\Qt\lib\Qt5Widgets.dll .\ImageCompressor\Release\ /y
 cmake --build ImageCompressor\ --config Release
--- execution.bat
+++ execution.bat
@@ -4,9 +4,9 @@
 ::ImageCompressor\Release\ImageCompressor.exe img/100_100.png output kmeans euclidean 6
 
 ::ImageCompressor\Release\ImageCompressor.exe img/100_100.png output dbscan euclidean 4
-::ImageCompressor\Release\ImageCompressor.exe img/680_618.png output dbscan manhattan 15
+ImageCompressor\Release\ImageCompressor.exe img/680_618.png output dbscan manhattan 15
 ::ImageCompressor\Release\ImageCompressor.exe img/100_100.png output dbscan euclidean 100
 ::ImageCompressor\Release\ImageCompressor.exe img/680_618.png output dbscan euclidean 100
 
 ::ImageCompressor\Release\ImageCompressor.exe img/1920_1080.png output dbscan manhattan 15
-ImageCompressor\Release\ImageCompressor.exe img/dragon.png output dbscan euclidean 32
+::ImageCompressor\Release\ImageCompressor.exe img/dragon.png output dbscan euclidean 32
